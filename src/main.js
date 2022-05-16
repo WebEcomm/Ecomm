8188806e02e6631ae86fd02af807c20f0bb8de49
MohandSL <msaidlhadj2@gmail.com>
2022-05-16 17:01:21 +0200

    Test Page Accueil

--- src/main.js
+++ src/main.js
@@ -1,22 +1,5 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-const app = createApp(App);
-
-app.use(router);
-
-app.mount("#app");
-
-// import Vue from 'vue';
-// import { App } from './App.vue';
-// import router from './router';
-
-// // Vue.config.productionTip = false;
-
-// new View({
-//     router,
-//     render: h => h(App)
-// }).$mount('#app')
+createApp(App).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,246 +1,96 @@
 <template>
-  <body>
-    <header class="site-header sticky-top py-1">
-      <nav
-        class="container d-flex flex-column flex-md-row justify-content-between"
-      >
-        <a class="py-2" href="#" aria-label="Product">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="d-block mx-auto"
-            role="img"
-            viewBox="0 0 24 24"
-          >
-            <title>Product</title>
-            <circle cx="12" cy="12" r="10" />
-            <path
-              d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"
-            />
-          </svg>
-        </a>
-        <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
-        <a class="py-2 d-none d-md-inline-block" href="#">Product</a>
-        <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
-        <a class="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
-        <a class="py-2 d-none d-md-inline-block" href="#">Support</a>
-        <a class="py-2 d-none d-md-inline-block" href="#">Pricing</a>
-        <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
-      </nav>
-    </header>
-
-    <main>
-      <div
-        class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light"
-      >
-        <div class="col-md-5 p-lg-5 mx-auto my-5">
-          <h1 class="display-4 fw-normal">Punny headline</h1>
-          <p class="lead fw-normal">
-            And an even wittier subheading to boot. Jumpstart your marketing
-            efforts with this example based on Apple’s marketing pages.
+  <section v-on:scroll.passive="scrollHeader" class="home">
+    <div class="container">
+      <!--  -->
+      <div>
+        <img 
+          src="@/assets/images/home.png" 
+          alt="home"
+          class="img"
+        />
+      </div>
+      <!--  -->
+      <div class="data">
+        <div class="header">
+          <h2 class="title">On ear</h2>
+          <h2 class="subtitle">Beats 3</h2>
+        </div>
+        <div class="footer">
+          <h2 class="title">Overview</h2>
+          <p class="description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem, aspernatur consectetur numquam nam assumenda omnis obcaecati reprehenderit tempore cum magnam accusantium blanditiis ratione voluptates tenetur perferendis minima aperiam quod, dolor asperiores placeat ut corrupti laborum. Eius ipsam molestiae deserunt optio!
           </p>
-          <a class="btn btn-outline-secondary" href="#">Coming soon</a>
-        </div>
-        <div class="product-device shadow-sm d-none d-md-block"></div>
-        <div
-          class="product-device product-device-2 shadow-sm d-none d-md-block"
-        ></div>
-      </div>
-
-      <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-        <div
-          class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden"
-        >
-          <div class="my-3 py-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
+          <div class="pricing">
+            <icon-button label="Add to Bage" icon="shopping-bag-line"/>
+            <div class="price">$299</div>
           </div>
-          <div
-            class="bg-light shadow-sm mx-auto"
-            style="width: 80%; height: 300px; border-radius: 21px 21px 0 0"
-          ></div>
-        </div>
-        <div
-          class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden"
-        >
-          <div class="my-3 p-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-          </div>
-          <div
-            class="bg-dark shadow-sm mx-auto"
-            style="width: 80%; height: 300px; border-radius: 21px 21px 0 0"
-          ></div>
         </div>
       </div>
-
-      <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-        <div
-          class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden"
-        >
-          <div class="my-3 p-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-          </div>
-          <div
-            class="bg-dark shadow-sm mx-auto"
-            style="width: 80%; height: 300px; border-radius: 21px 21px 0 0"
-          ></div>
-        </div>
-        <div
-          class="bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden"
-        >
-          <div class="my-3 py-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-          </div>
-          <div
-            class="bg-light shadow-sm mx-auto"
-            style="width: 80%; height: 300px; border-radius: 21px 21px 0 0"
-          ></div>
-        </div>
-      </div>
-
-      <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-        <div
-          class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden"
-        >
-          <div class="my-3 p-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-          </div>
-          <div
-            class="bg-body shadow-sm mx-auto"
-            style="width: 80%; height: 300px; border-radius: 21px 21px 0 0"
-          ></div>
-        </div>
-        <div
-          class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden"
-        >
-          <div class="my-3 py-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-          </div>
-          <div
-            class="bg-body shadow-sm mx-auto"
-            style="width: 80%; height: 300px; border-radius: 21px 21px 0 0"
-          ></div>
-        </div>
-      </div>
-
-      <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-        <div
-          class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden"
-        >
-          <div class="my-3 p-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-          </div>
-          <div
-            class="bg-body shadow-sm mx-auto"
-            style="width: 80%; height: 300px; border-radius: 21px 21px 0 0"
-          ></div>
-        </div>
-        <div
-          class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden"
-        >
-          <div class="my-3 py-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-          </div>
-          <div
-            class="bg-body shadow-sm mx-auto"
-            style="width: 80%; height: 300px; border-radius: 21px 21px 0 0"
-          ></div>
-        </div>
-      </div>
-    </main>
-
-    <footer class="container py-5">
-      <div class="row">
-        <div class="col-12 col-md">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="d-block mb-2"
-            role="img"
-            viewBox="0 0 24 24"
-          >
-            <title>Product</title>
-            <circle cx="12" cy="12" r="10" />
-            <path
-              d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"
-            />
-          </svg>
-          <small class="d-block mb-3 text-muted">&copy; 2017–2021</small>
-        </div>
-        <div class="col-6 col-md">
-          <h5>Features</h5>
-          <ul class="list-unstyled text-small">
-            <li><a class="link-secondary" href="#">Cool stuff</a></li>
-            <li><a class="link-secondary" href="#">Random feature</a></li>
-            <li><a class="link-secondary" href="#">Team feature</a></li>
-            <li><a class="link-secondary" href="#">Stuff for developers</a></li>
-            <li><a class="link-secondary" href="#">Another one</a></li>
-            <li><a class="link-secondary" href="#">Last time</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-md">
-          <h5>Resources</h5>
-          <ul class="list-unstyled text-small">
-            <li><a class="link-secondary" href="#">Resource name</a></li>
-            <li><a class="link-secondary" href="#">Resource</a></li>
-            <li><a class="link-secondary" href="#">Another resource</a></li>
-            <li><a class="link-secondary" href="#">Final resource</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-md">
-          <h5>Resources</h5>
-          <ul class="list-unstyled text-small">
-            <li><a class="link-secondary" href="#">Business</a></li>
-            <li><a class="link-secondary" href="#">Education</a></li>
-            <li><a class="link-secondary" href="#">Government</a></li>
-            <li><a class="link-secondary" href="#">Gaming</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-md">
-          <h5>About</h5>
-          <ul class="list-unstyled text-small">
-            <li><a class="link-secondary" href="#">Team</a></li>
-            <li><a class="link-secondary" href="#">Locations</a></li>
-            <li><a class="link-secondary" href="#">Privacy</a></li>
-            <li><a class="link-secondary" href="#">Terms</a></li>
-          </ul>
-        </div>
-      </div>
-    </footer>
-  </body>
+    </div>
+  </section>
 </template>
 
-<style>
-.bd-placeholder-img {
-  font-size: 1.125rem;
-  text-anchor: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  user-select: none;
+<script setup>
+//import { defineEmits } from 'vue';
+import { IconButton } from '@/components';
+const scrollHeader = () => {
+  console.log("scroll")
 }
+//const emit = defineEmits(['scrollHeader'])
+</script>
 
-@media (min-width: 768px) {
-  .bd-placeholder-img-lg {
-    font-size: 3.5rem;
+<style lang="scss">
+/*========== VARIABLES ==========*/
+@use '@/assets/styles' as *;
+
+.home {
+  margin-top: $size-header-height;
+  .container {
+
+    .img {
+      position: absolute;
+      top: -3*$size-biggest;
+      right: $size-xxl;
+      width: 250px;
+      //z-index: $font-fixed;
+    }
+    .data {
+      display: grid;
+      margin-top: $size-biggest;
+      .header {
+        display: flex;
+        flex-direction: column;
+        position: relative;
+        .title { 
+          @include color-gradient;
+          margin: 0; 
+        }
+        .subtitle { 
+          transform: translate(-$size-biggest,-2*$size-xl);
+          text-align: center;
+          font-size: 2*$size-l;
+        }
+      }
+      .footer {
+        .title { 
+          text-align: left;
+          font-size: $size-xl; 
+          margin-bottom: $size-m; 
+        }
+        .description { margin-bottom: 2*$size-xl; }
+        .pricing { 
+          display: flex;
+          align-items: center;
+          column-gap: $size-s;
+          width: max-content;
+          padding-right: $size-s;
+          border-radius: $size-s;
+          background-color: $color-black;
+          &:hover { background-color: $color-black-alt; }
+          .btn { background: transparent; }
+        }
+      }
+    }
   }
 }
 </style>
