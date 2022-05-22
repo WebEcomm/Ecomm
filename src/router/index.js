0f58f0d44666b3f381f74858af323c844497f658
Ririlost <Ririlost@gmail.com>
2022-05-23 02:12:08 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const router = createRouter({
     {
       path: "/login",
       name: "login",
-      component: () => import("../components/Login.vue"),
+      component: () => import("../views/Login.vue"),
     },
     {
       path: "/register",
